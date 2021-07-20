958ab7455ca801ff90ef34a31eb39273f4b41891
gODealOAple <maksim.math@gmail.com>
2021-07-20 20:05:39 +0500

    Init commit

---
+++ dotnet/resources/Server/Main.cs
@@ -0,0 +1,21 @@
+﻿using GTANetworkAPI;
+using Newtonsoft.Json;
+using System;
+
+namespace Server
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            Console.WriteLine(JsonConvert.SerializeObject("OK"));
+        }
+
+        [ServerEvent(Event.PlayerConnected)]
+        public void OnPlayerConnected(Player player)
+        {
+        }
+    }
+
+
+}
